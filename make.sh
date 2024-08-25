5f2ecea330806897ec5821d8bb05d643a41ebee4
Adhityaa Chandrasekar <adtac@subtrace.dev>
2024-08-25 18:43:11 -0400

    make.sh: remove obsolete make target cmd:clickhouse

--- make.sh
+++ make.sh
@@ -27,38 +27,6 @@ cmd:proto() {
     --go_out=. --go_opt=paths=source_relative
 }
 
-cmd:clickhouse() {
-  case $1 in
-    start)
-      mkdir -p "${format_schemas_dir}"
-      docker run -d --rm --name subtrace_clickhouse \
-        -u $(id -u):$(id -g) -e CLICKHOUSE_UID=0 -e CLICKHOUSE_GID=0 \
-        -p 127.0.0.1:8123:8123 \
-        -p 127.0.0.1:9000:9000 \
-        -e CLICKHOUSE_DB=subtrace \
-        -v subtrace_clickhouse:/var/lib/clickhouse/ \
-        -v ${format_schemas_dir}:/var/lib/clickhouse/format_schemas/:rw \
-        clickhouse/clickhouse-server:23
-      ;;
-    wait)
-      sleep 0.5
-      ;;
-    clear)
-      docker exec -it subtrace_clickhouse clickhouse-client --host 127.0.0.1 --port 9000 --query "drop database subtrace" || true
-      docker exec -it subtrace_clickhouse clickhouse-client --host 127.0.0.1 --port 9000 --query "create database subtrace"
-      ;;
-    stop)
-      docker stop subtrace_clickhouse
-      ;;
-    query)
-      docker exec -i subtrace_clickhouse clickhouse-client --database subtrace --format PrettyCompactMonoBlock
-      ;;
-    "")
-      docker exec -it subtrace_clickhouse clickhouse-client --database subtrace
-      ;;
-  esac
-}
-
 main() {
   if [[ "$1" != "" ]]; then
     cmd=$(echo "$1" | tr ':' ' ')
