a5ba9f3c81e8e5082ea4e1dd76983fb4a07264e9
Adhityaa Chandrasekar <adtac@subtrace.dev>
2024-09-03 16:07:06 -0400

    rpc: add GetEndpoint method

--- rpc/endpoint.go
+++ rpc/endpoint.go
@@ -6,21 +6,24 @@ import (
 	"strings"
 )
 
+func GetEndpoint() string {
+	if endpoint := os.Getenv("SUBTRACE_ENDPOINT"); endpoint != "" {
+		return endpoint
+	}
+	return "subtrace.dev"
+}
+
 // Format takes a path and returns its URL. For example, given "/api/GetSelf",
 // Format will return "https://subtrace.dev/api/GetSelf" as the URL.
 //
 // The SUBTRACE_ENDPOINT environment variable can be used to override the
 // domain name used.
 func Format(path string) string {
-	endpoint := os.Getenv("SUBTRACE_ENDPOINT")
-	if endpoint == "" {
-		endpoint = "subtrace.dev"
-	}
 	if path == "" {
 		path = "/"
 	}
 	if !strings.HasPrefix(path, "/") { // TODO: is this too relaxed?
 		path = "/" + path
 	}
-	return fmt.Sprintf("https://%s%s", endpoint, path)
+	return fmt.Sprintf("https://%s%s", GetEndpoint(), path)
 }
