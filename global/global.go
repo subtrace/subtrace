8f58b64ccec8842ee43c19df8df201675163c9d2
Sachin Sridhar <sachin@subtrace.dev>
2025-01-26 03:08:38 +0000

    Stats: add cpu and memory info to tags

--- global/global.go
+++ global/global.go
@@ -4,9 +4,11 @@ import (
 	"subtrace.dev/config"
 	"subtrace.dev/devtools"
 	"subtrace.dev/event"
+	"subtrace.dev/stats"
 )
 
 type Global struct {
+	Stats         *stats.Stats
 	Config        *config.Config
 	Devtools      *devtools.Server
 	EventTemplate *event.Event
