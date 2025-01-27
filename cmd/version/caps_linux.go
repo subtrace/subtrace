8b3bc0e0a799b8da34c32b213e11cbb594b88caf
Adhityaa Chandrasekar <adtac@subtrace.dev>
2025-01-26 17:18:24 -0800

    version: move capability string list to caps_linux.go

--- cmd/version/caps_linux.go
+++ cmd/version/caps_linux.go
@@ -6,6 +6,50 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+var capNames = []string{
+	"chown",
+	"dac_override",
+	"dac_read_search",
+	"fowner",
+	"fsetid",
+	"kill",
+	"setgid",
+	"setuid",
+	"setpcap",
+	"linux_immutable",
+	"net_bind_service",
+	"net_broadcast",
+	"net_admin",
+	"net_raw",
+	"ipc_lock",
+	"ipc_owner",
+	"sys_module",
+	"sys_rawio",
+	"sys_chroot",
+	"sys_ptrace",
+	"sys_pacct",
+	"sys_admin",
+	"sys_boot",
+	"sys_nice",
+	"sys_resource",
+	"sys_time",
+	"sys_tty_config",
+	"mknod",
+	"lease",
+	"audit_write",
+	"audit_control",
+	"setfcap",
+	"mac_override",
+	"mac_admin",
+	"syslog",
+	"wake_alarm",
+	"block_suspend",
+	"audit_read",
+	"perfmon",
+	"bpf",
+	"checkpoint_restore",
+}
+
 func GetEffectiveCaps() string {
 	effectiveCaps := "unknown"
 	hdr := unix.CapUserHeader{Version: unix.LINUX_CAPABILITY_VERSION_3}
