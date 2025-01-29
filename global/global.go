5c1f6e2d3f90b94c33f2ca289417aed62dd629b0
Adhityaa Chandrasekar <adtac@subtrace.dev>
2025-01-29 12:50:32 -0800

    stats: refactor into a single function

--- global/global.go
+++ global/global.go
@@ -3,11 +3,9 @@ package global
 import (
 	"subtrace.dev/config"
 	"subtrace.dev/devtools"
-	"subtrace.dev/stats"
 )
 
 type Global struct {
-	Stats    *stats.Stats
 	Config   *config.Config
 	Devtools *devtools.Server
 }
--- stats/stats_linux.go
+++ stats/stats_linux.go
@@ -3,164 +3,76 @@ package stats
 import (
 	"context"
 	"fmt"
-	"log/slog"
 	"os"
+	"runtime"
 	"strings"
 	"sync"
 	"time"
 )
 
-type Stats struct {
-	cpuCount string
+var mu sync.RWMutex
+var data = make(map[string]string)
 
-	loadAvg1Min  string
-	loadAvg5Min  string
-	loadAvg15Min string
-
-	memTotal     string
-	memFree      string
-	memAvailable string
-
-	mu sync.RWMutex
-}
-
-func New(ctx context.Context) *Stats {
-	s := new(Stats)
-
-	count, err := getCPUCount()
-	if err != nil {
-		// This isn't critical, so we just log.
-		slog.Info("failed to get cpu count", "err", err)
-		s.cpuCount = ""
-	} else {
-		s.cpuCount = fmt.Sprintf("%d", count)
+func Loop(ctx context.Context) {
+	select {
+	case <-ctx.Done():
+		return
+	default:
+		tick()
 	}
 
-	go func() {
-		ticker := time.NewTicker(time.Second)
-		defer ticker.Stop()
-		for {
-			select {
-			case <-ticker.C:
-				s.tick()
-			case <-ctx.Done():
-				return
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			tick()
+		}
+	}
+}
+
+func tick() {
+	m := make(map[string]string)
+
+	m["subtrace_linux_cpu_count"] = fmt.Sprintf("%d", runtime.NumCPU())
+
+	if b, err := os.ReadFile("/proc/loadavg"); err == nil {
+		if fields := strings.Split(string(b), " "); len(fields) >= 3 {
+			m["subtrace_linux_cpu_load"] = strings.Join(fields[0:3], " ")
+		}
+	}
+
+	if b, err := os.ReadFile("/proc/meminfo"); err == nil {
+		for _, line := range strings.Split(string(b), "\n") {
+			if fields := strings.Fields(line); len(fields) >= 2 {
+				val := strings.Join(fields[1:], " ")
+				switch strings.TrimSuffix(fields[0], ":") {
+				case "MemTotal":
+					m["subtrace_linux_mem_total"] = val
+				case "MemAvailable":
+					m["subtrace_linux_mem_available"] = val
+				}
 			}
 		}
-	}()
-
-	return s
-
-}
-
-func (s *Stats) tick() {
-	loadAvg1Min, loadAvg5Min, loadAvg15Min, err := getLoadAvgInfo()
-	if err != nil {
-		// This isn't critical, so we just log.
-		slog.Info("failed to get loadavg", "err", err)
-		loadAvg1Min = ""
-		loadAvg5Min = ""
-		loadAvg15Min = ""
 	}
 
-	memTotal, memFree, memAvailable, err := getMemInfo()
-	if err != nil {
-		// This isn't critical, so we just log.
-		slog.Info("failed to get meminfo", "err", err)
-		memTotal = ""
-		memFree = ""
-		memAvailable = ""
-	}
-
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	s.loadAvg1Min = loadAvg1Min
-	s.loadAvg5Min = loadAvg5Min
-	s.loadAvg15Min = loadAvg15Min
-	s.memTotal = memTotal
-	s.memFree = memFree
-	s.memAvailable = memAvailable
-
-}
-
-func (s *Stats) GetStatsTags() map[string]string {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-
-	return map[string]string{
-		"subtrace_linux_cpu_count":             s.cpuCount,
-		"subtrace_linux_loadavg_1_min":         s.loadAvg1Min,
-		"subtrace_linux_loadavg_5_min":         s.loadAvg5Min,
-		"subtrace_linux_loadavg_15_min":        s.loadAvg15Min,
-		"subtrace_linux_meminfo_mem_total":     s.memTotal,
-		"subtrace_linux_meminfo_mem_free":      s.memFree,
-		"subtrace_linux_meminfo_mem_available": s.memAvailable,
+	mu.Lock()
+	defer mu.Unlock()
+	for k, v := range m {
+		data[k] = v
 	}
 }
 
-func getCPUCount() (int, error) {
-	b, err := os.ReadFile("/proc/cpuinfo")
-	if err != nil {
-		return 0, fmt.Errorf("read /proc/cpuinfo: %w", err)
+func Load() map[string]string {
+	mu.RLock()
+	defer mu.RUnlock()
+
+	m := make(map[string]string)
+	for k, v := range data {
+		m[k] = v
 	}
-
-	blocks := strings.Split(string(b), "\n\n")
-	return len(blocks) - 1, nil
-}
-
-// From [1]:
-//
-// /proc/loadavg
-// The first three fields in this file are load average figures giving the number
-// of jobs in the run queue (state R) or waiting for disk I/O (state D) averaged
-// over 1, 5, and 15 minutes.
-//
-// [1] https://man7.org/linux/man-pages/man5/proc_loadavg.5.html
-func getLoadAvgInfo() (string, string, string, error) {
-	b, err := os.ReadFile("/proc/loadavg")
-	if err != nil {
-		return "", "", "", fmt.Errorf("read /proc/loadavg: %w", err)
-	}
-
-	values := strings.Split(string(b), " ")
-	if len(values) < 3 {
-		return "", "", "", fmt.Errorf("not enough info in /proc/loadavg, expected at least 3 values but got %d", len(values))
-	}
-
-	return values[0], values[1], values[2], nil
-
-}
-
-// See [1] for details about the /proc/meminfo file.
-//
-// [1] https://man7.org/linux/man-pages/man5/proc_meminfo.5.html
-func getMemInfo() (string, string, string, error) {
-	b, err := os.ReadFile("/proc/meminfo")
-	if err != nil {
-		return "", "", "", fmt.Errorf("read: %w", err)
-	}
-
-	var memTotal, memFree, memAvailable string
-	lines := strings.Split(string(b), "\n")
-
-	for _, l := range lines {
-		words := strings.Fields(l)
-		if len(words) < 2 {
-			continue
-		}
-
-		key := words[0]
-		value := strings.Join(words[1:], " ")
-		switch key {
-		case "MemTotal:":
-			memTotal = value
-		case "MemFree:":
-			memFree = value
-		case "MemAvailable:":
-			memAvailable = value
-		}
-	}
-
-	return memTotal, memFree, memAvailable, nil
+	return m
 }
